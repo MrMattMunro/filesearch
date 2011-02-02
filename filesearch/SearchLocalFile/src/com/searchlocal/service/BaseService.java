7aec6518180fddec21882effefd2bf87f88f9844
qianjinfu <qianjinfu@3ac28aef-54fb-d8bb-2f82-0eb7bc437e39>
2011-02-02 08:51:37 +0000

    修改注释

--- filesearch/SearchLocalFile/src/com/searchlocal/service/BaseService.java
+++ filesearch/SearchLocalFile/src/com/searchlocal/service/BaseService.java
@@ -1,23 +1,49 @@
+/**
+ * $RCSfile: BaseService.java
+ * $Revision: 1.0
+ * $Date: Jan 30, 2011
+ *
+ * Copyright (C) 2010 SlFile, Inc. All rights reserved.
+ *
+ * This software is the proprietary information of SlFile, Inc.
+ * Use is subject to license terms.
+ */
 package com.searchlocal.service;
 
 import com.searchlocal.dao.BaseDao;
 import com.searchlocal.exception.DBException;
 import com.searchlocal.exception.LogicException;
 
+/**
+ * 服务类基类
+ * 
+ * <p>Title: 服务类基类</p>
+ * <p>Description: </p>
+ * <p>site: www.slfile.net</p>
+ * @author changsong:qianjinfu@gmail.com
+ * @version 1.0
+ */
 public class BaseService {
 
-	public boolean createDataBase(String namespace) throws DBException,
-			LogicException {
+	/**
+	 * 创建数据库
+	 * 
+	 * @param namespace 数据库名
+	 * @throws DBException
+	 * @throws LogicException
+	 */
+	public boolean createDataBase(String namespace) throws DBException, LogicException {
 		return BaseDao.createDataBase(namespace);
 	}
 
-	public boolean dropDataBase(String namespace) throws DBException,
-			LogicException {
-		return BaseDao.dropDataBase(namespace);
-	}
-
-	public boolean createFileTable(String namespace) throws DBException,
-			LogicException {
+	/**
+	 * 删除数据库
+	 * 
+	 * @param namespace 数据库名
+	 * @throws DBException
+	 * @throws LogicException
+	 */
+	public boolean dropDataBase(String namespace) throws DBException, LogicException {
 		return BaseDao.dropDataBase(namespace);
 	}
 
--- filesearch/SearchLocalFile/src/com/searchlocal/service/ChmService.java
+++ filesearch/SearchLocalFile/src/com/searchlocal/service/ChmService.java
@@ -1,10 +1,15 @@
+/**
+ * $RCSfile: ChmService.java
+ * $Revision: 1.0
+ * $Date: Jan 30, 2011
+ *
+ * Copyright (C) 2010 SlFile, Inc. All rights reserved.
+ *
+ * This software is the proprietary information of SlFile, Inc.
+ * Use is subject to license terms.
+ */
 package com.searchlocal.service;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.sql.Timestamp;
-import java.util.Iterator;
 import java.util.List;
 
 import com.searchlocal.bean.ChmFileBean;
@@ -13,82 +18,44 @@ import com.searchlocal.dao.ChmDao;
 import com.searchlocal.exception.DBException;
 import com.searchlocal.exception.LogicException;
 import com.searchlocal.filereader.ChmReader;
-import com.searchlocal.util.StringUtils;
+import com.searchlocal.lucene.IndexBeanList;
 
+/**
+ * Chm文件服务类
+ * 
+ * <p>Title: Chm文件服务类</p>
+ * <p>Description: </p>
+ * <p>site: www.slfile.net</p>
+ * @author changsong:qianjinfu@gmail.com
+ * @version 1.0
+ */
 public class ChmService {
 
-	public boolean execBatch(String namespace, String chmpath) throws DBException,
-			LogicException {
-		ChmDao chmDao = new ChmDao();
-		chmpath = StringUtils.editFilePath(chmpath);
-
-		chmDao.execbatch(chmpath, namespace);
-		File chmfile = new File(chmpath);
-		// 删除batch的数据文件
-		if (chmfile.exists()) {
-			chmfile.delete();
-		}
-		return true;
-	}
-
-	public int createBatchFile(ChmFileBean chmbean, String namespace,
-			int fileClassify) throws DBException, LogicException {
-		List fileBeanList;
+	/**
+	 * 对chm文件进行建立索引
+	 * 
+	 * @param chmbean chm文件Bean
+	 * @param namespace 数据库名
+	 * @throws DBException
+	 * @throws LogicException
+	 */
+	public int createIndex(ChmFileBean chmbean, String namespace)
+			throws DBException, LogicException {
+		List<ChmFileBean> fileBeanList;
 		ChmReader chmReader = new ChmReader();
-		fileBeanList = chmReader.getChmFile(chmbean.getPath());
-
-		File wordfile = new File(Constant.datapath + Constant.chmdatapath + fileClassify + Constant.suffixname);
-		FileOutputStream out = null;
-		try {
-			if (!wordfile.exists()) {
-				wordfile.createNewFile();
-			}
-			out = new FileOutputStream(wordfile, true);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		for (Iterator iter = fileBeanList.iterator(); iter.hasNext();) {
-			ChmFileBean element = (ChmFileBean) iter.next();
-			String temp = "";
-			temp = chmbean.getFilename();
-			String path = chmbean.getPath();
-			path = StringUtils.editFilePathForBatch(path);
-			temp = temp + "," + path;
-			temp = temp + "," + new Timestamp(chmbean.getLastmodify());
-			String content = element.getContent();
-			content = StringUtils.replaceRN(content);
-			temp = temp + "," + content;
-			temp = temp + "," + String.valueOf(element.getCatalogname());
-			temp = temp + "\r\n";
-			try {
-				out.write(temp.getBytes("utf-8"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		try {
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		fileBeanList = chmReader.getChmFile(chmbean);
+		IndexBeanList.makeindex(namespace, Constant.FileNameClassify.CHM, fileBeanList);
 		return fileBeanList.size();
 	}
 
-	public boolean insertChmRecord(ChmFileBean chmbean, String namespace)
-			throws DBException, LogicException {
-		ChmDao ChmDao;
-		ChmDao = new ChmDao();
-		ChmReader chmReader = new ChmReader();
-		List fileBeanList = chmReader.getChmFile(chmbean.getPath());
-		return ChmDao.insertChmRecord(fileBeanList, chmbean.getPath(), chmbean
-				.getLastmodify(), chmbean.getFilename(), namespace);
-	}
-
-	public boolean createChmTable(String namespace) throws DBException,
-			LogicException {
+	/**
+	 * 创建Chm文件表
+	 * 
+	 * @param namespace 数据库名
+	 * @throws DBException
+	 * @throws LogicException
+	 */
+	public boolean createChmTable(String namespace) throws DBException, LogicException {
 		ChmDao ChmDao;
 		ChmDao = new ChmDao();
 		return ChmDao.createChmtable(namespace);
