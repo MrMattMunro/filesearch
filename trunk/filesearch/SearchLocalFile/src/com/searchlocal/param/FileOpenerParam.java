2f6c3794d09adb882df39c9038b17a00d7054c6e
qianjinfu <qianjinfu@3ac28aef-54fb-d8bb-2f82-0eb7bc437e39>
2011-02-03 02:45:27 +0000

    修改注释

--- trunk/filesearch/SearchLocalFile/src/com/searchlocal/param/FileOpenerParam.java
+++ trunk/filesearch/SearchLocalFile/src/com/searchlocal/param/FileOpenerParam.java
@@ -1,107 +1,150 @@
+/**
+ * $RCSfile: FileOpenerParam.java
+ * $Revision: 1.0
+ * $Date: Jan 30, 2011
+ *
+ * Copyright (C) 2010 SlFile, Inc. All rights reserved.
+ *
+ * This software is the proprietary information of SlFile, Inc.
+ * Use is subject to license terms.
+ */
 package com.searchlocal.param;
 
+/**
+ * 打开文件Exe文件位置
+ * 
+ * <p>Title: 打开文件Exe文件位置</p>
+ * <p>Description: </p>
+ * <p>site: www.slfile.net</p>
+ * @author changsong:qianjinfu@gmail.com
+ * @version 1.0
+ */
 public class FileOpenerParam {
 
+	/** Html打开软件路径 */
 	private String iedir;
 
+	/** Word打开软件路径 */
 	private String worddir;
 
+	/** Excel打开软件路径 */
 	private String exceldir;
 
+	/** Ppt打开软件路径 */
 	private String pptdir;
 
+	/**Pdf打开软件路径 */
 	private String pdfdir;
 
+	/**Txt打开软件路径 */
 	private String txtdir;
 
 	/**
-	 * @return Returns the exceldir.
-	 */
-	public String getExceldir() {
-		return exceldir;
-	}
-
-	/**
-	 * @param exceldir
-	 *            The exceldir to set.
-	 */
-	public void setExceldir(String exceldir) {
-		this.exceldir = exceldir;
-	}
-
-	/**
-	 * @return Returns the iedir.
+	 * 取得iedir
+	 * 
+	 * @return String iedir 
 	 */
 	public String getIedir() {
 		return iedir;
 	}
 
 	/**
-	 * @param iedir
-	 *            The iedir to set.
+	 * 设定iedir 
+	 * 
+	 * @param String iedir 
 	 */
 	public void setIedir(String iedir) {
 		this.iedir = iedir;
 	}
 
 	/**
-	 * @return Returns the pdfdir.
-	 */
-	public String getPdfdir() {
-		return pdfdir;
-	}
-
-	/**
-	 * @param pdfdir
-	 *            The pdfdir to set.
-	 */
-	public void setPdfdir(String pdfdir) {
-		this.pdfdir = pdfdir;
-	}
-
-	/**
-	 * @return Returns the txtdir.
-	 */
-	public String getTxtdir() {
-		return txtdir;
-	}
-
-	/**
-	 * @param txtdir
-	 *            The txtdir to set.
-	 */
-	public void setTxtdir(String txtdir) {
-		this.txtdir = txtdir;
-	}
-
-	/**
-	 * @return Returns the worddir.
+	 * 取得worddir
+	 * 
+	 * @return String worddir 
 	 */
 	public String getWorddir() {
 		return worddir;
 	}
 
 	/**
-	 * @param worddir
-	 *            The worddir to set.
+	 * 设定worddir 
+	 * 
+	 * @param String worddir 
 	 */
 	public void setWorddir(String worddir) {
 		this.worddir = worddir;
 	}
 
 	/**
-	 * @return Returns the pptdir.
+	 * 取得exceldir
+	 * 
+	 * @return String exceldir 
+	 */
+	public String getExceldir() {
+		return exceldir;
+	}
+
+	/**
+	 * 设定exceldir 
+	 * 
+	 * @param String exceldir 
+	 */
+	public void setExceldir(String exceldir) {
+		this.exceldir = exceldir;
+	}
+
+	/**
+	 * 取得pptdir
+	 * 
+	 * @return String pptdir 
 	 */
 	public String getPptdir() {
 		return pptdir;
 	}
 
 	/**
-	 * @param pptdir
-	 *            The pptdir to set.
+	 * 设定pptdir 
+	 * 
+	 * @param String pptdir 
 	 */
 	public void setPptdir(String pptdir) {
 		this.pptdir = pptdir;
+	}
+
+	/**
+	 * 取得pdfdir
+	 * 
+	 * @return String pdfdir 
+	 */
+	public String getPdfdir() {
+		return pdfdir;
+	}
+
+	/**
+	 * 设定pdfdir 
+	 * 
+	 * @param String pdfdir 
+	 */
+	public void setPdfdir(String pdfdir) {
+		this.pdfdir = pdfdir;
+	}
+
+	/**
+	 * 取得txtdir
+	 * 
+	 * @return String txtdir 
+	 */
+	public String getTxtdir() {
+		return txtdir;
+	}
+
+	/**
+	 * 设定txtdir 
+	 * 
+	 * @param String txtdir 
+	 */
+	public void setTxtdir(String txtdir) {
+		this.txtdir = txtdir;
 	}
 
 }
--- trunk/filesearch/SearchLocalFile/src/com/searchlocal/param/FileParam.java
+++ trunk/filesearch/SearchLocalFile/src/com/searchlocal/param/FileParam.java
@@ -1,57 +1,84 @@
+/**
+ * $RCSfile: FileParam.java
+ * $Revision: 1.0
+ * $Date: Jan 30, 2011
+ *
+ * Copyright (C) 2010 SlFile, Inc. All rights reserved.
+ *
+ * This software is the proprietary information of SlFile, Inc.
+ * Use is subject to license terms.
+ */
 package com.searchlocal.param;
 
+/**
+ * 文件列表
+ * 
+ * <p>Title: 文件列表</p>
+ * <p>Description: </p>
+ * <p>site: www.slfile.net</p>
+ * @author changsong:qianjinfu@gmail.com
+ * @version 1.0
+ */
 public class FileParam {
 
+	/**  文件路径 */
 	private String path;
 
-	Long lastModify;
+	/**  最后修改时间 */
+	private Long lastModify;
 
-	String error;
-
-	/**
-	 * @return Returns the lastModify.
-	 */
-	public Long getLastModify() {
-		return lastModify;
-	}
+	/**  是否存在错误 */
+	private String error;
 
 	/**
-	 * @param lastModify
-	 *            The lastModify to set.
-	 */
-	public void setLastModify(Long lastModify) {
-		this.lastModify = lastModify;
-	}
-
-	/**
-	 * @return Returns the path.
+	 * 取得path
+	 * 
+	 * @return String path 
 	 */
 	public String getPath() {
 		return path;
 	}
 
 	/**
-	 * @param path
-	 *            The path to set.
+	 * 设定path 
+	 * 
+	 * @param String path 
 	 */
 	public void setPath(String path) {
 		this.path = path;
 	}
 
-	public FileParam() {
-		super();
-		// TODO Auto-generated constructor stub
+	/**
+	 * 取得lastModify
+	 * 
+	 * @return Long lastModify 
+	 */
+	public Long getLastModify() {
+		return lastModify;
 	}
 
 	/**
-	 * @return Returns the error.
+	 * 设定lastModify 
+	 * 
+	 * @param Long lastModify 
+	 */
+	public void setLastModify(Long lastModify) {
+		this.lastModify = lastModify;
+	}
+
+	/**
+	 * 取得error
+	 * 
+	 * @return String error 
 	 */
 	public String getError() {
 		return error;
 	}
 
 	/**
-	 * @param error The error to set.
+	 * 设定error 
+	 * 
+	 * @param String error 
 	 */
 	public void setError(String error) {
 		this.error = error;
