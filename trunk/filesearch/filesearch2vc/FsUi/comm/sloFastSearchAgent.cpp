eb6fbd39bd6d80c0159e441c8b69bc2ce4e75a7a
maxskyworld <maxskyworld@3ac28aef-54fb-d8bb-2f82-0eb7bc437e39>
2011-06-18 03:06:38 +0000

    1 修改快速查找页面中去“全部”字段错误的bug

--- trunk/filesearch/filesearch2vc/FsUi/comm/sloFastSearchAgent.cpp
+++ trunk/filesearch/filesearch2vc/FsUi/comm/sloFastSearchAgent.cpp
@@ -236,7 +236,7 @@ void sloFastSearchAgent::ClearRecList()
 
 int sloFastSearchAgent::GetPathIndex(char* szPath)
 {
-	if (strcmp(szPath, g_lag.LoadString("title.fastsearch")) == 0)
+	if (strcmp(szPath, g_lag.LoadString("label.fastserlist")) == 0)
 	{
 		return 0;
 	}
