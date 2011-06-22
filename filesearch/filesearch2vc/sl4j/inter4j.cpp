b358b1809cfc09d86f20e065401df0c4339cabd0
maxskyworld <maxskyworld@3ac28aef-54fb-d8bb-2f82-0eb7bc437e39>
2011-06-22 15:12:02 +0000

    1 执行进程时，先杀掉java。exe和update。exe

--- filesearch/filesearch2vc/sl4j/inter4j.cpp
+++ filesearch/filesearch2vc/sl4j/inter4j.cpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "StdAfx.h"
+#include <TLHELP32.H>
+
+void slTerminateProcess(char* pProcessName);
 
 DWORD __stdcall MsgBox(char* pText, char* pCaption, unsigned int uType)
 {
@@ -8,7 +11,37 @@ DWORD __stdcall MsgBox(char* pText, char* pCaption, unsigned int uType)
 
 DWORD __stdcall DoExe(char* szPath)
 {
+
+	//查找java，cmd进程，并结束掉
+	slTerminateProcess("java.exe");
+	slTerminateProcess("update.exe");
+
+	//然后在执行相应的进程
 	ShellExecute(NULL, "open",szPath, NULL,NULL, SW_SHOWNORMAL); 
 
 	return 0;
+}
+
+void slTerminateProcess(char* pProcessName)
+{
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(pe32);//为让系统知道使用的结构是哪个，故要求大小
+	HANDLE hexit;
+	//HANDLE htask;
+	BOOL btest;
+	HANDLE hsnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);//遍历系统进程
+	BOOL bMore = ::Process32First(hsnapshot,&pe32);
+	while(bMore)
+	{
+		if(strcmp(pe32.szExeFile, pProcessName)==0)
+		{
+			hexit = ::OpenProcess(PROCESS_ALL_ACCESS,FALSE,pe32.th32ProcessID);
+			btest = ::TerminateProcess(hexit,0);//结束进程
+			if(!btest)
+			{
+				continue;
+			}
+		}
+		bMore = ::Process32Next(hsnapshot,&pe32);
+	}
 }
