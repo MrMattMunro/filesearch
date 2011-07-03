1996aac42ec29171cc9461634c3805a99611e07b
maxskyworld <maxskyworld@3ac28aef-54fb-d8bb-2f82-0eb7bc437e39>
2011-07-03 02:00:59 +0000

    1 修改快速查找页面无法打开文件名路径带空格的文件，文件全路径需要加双引号“”

--- trunk/filesearch/filesearch2vc/FsUi/FsUiTest/resource.h
+++ trunk/filesearch/filesearch2vc/FsUi/FsUiTest/resource.h
@@ -17,6 +17,7 @@
 #define IDC_BUTTON7                     1007
 #define IDC_BUTTON8                     1008
 #define IDC_BUTTON9                     1009
+#define IDC_BUTTON10                    1010
 
 // Next default values for new objects
 // 
@@ -24,7 +25,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
