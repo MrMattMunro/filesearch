e2bd1172685c3ac3b02bf8a06e0249bf14c999e9
maxskyworld <maxskyworld@3ac28aef-54fb-d8bb-2f82-0eb7bc437e39>
2011-08-15 13:28:58 +0000

    1 合并comm。h文件 2 增加网址超链接

--- trunk/filesearch/filesearch2vc/FsUi/comm/comm.h
+++ trunk/filesearch/filesearch2vc/FsUi/comm/comm.h
@@ -1,5 +1,8 @@
 #if !defined(AFX_COMM__INCLUDED_)
 #define AFX_COMM__INCLUDED_
+#include <vector>
+#include <string>
+using namespace std;
 
 typedef enum{
 	lag_chinese = 0,
@@ -7,7 +10,46 @@ typedef enum{
 	lag_engish
 }LANGUAGE;
 
+enum File_Action{
+	FILE_UNKNOW = 0,
+	FILE_ADD,
+	FILE_MODIFYED,
+	FILE_REMOVED,
+	FILE_NAMECHANGE
+};
+
+#define	DEFAULT_DB_NAME		"commoninfo"
+
+
 #pragma pack(push, 1)
+
+
+typedef struct _File_Action_Log
+{
+	File_Action FileActon;
+	char szDbName[MAX_PATH];
+	char szSrcName[MAX_PATH*2];
+	char szDesName[MAX_PATH];
+	char szLogTime[30];
+	_File_Action_Log()
+	{
+		FileActon = FILE_UNKNOW;
+		memset(szDbName, NULL, MAX_PATH);
+		memset(szSrcName,NULL, MAX_PATH);
+		memset(szDesName,NULL, MAX_PATH);
+		memset(szLogTime, NULL, 30);
+	}
+	
+}File_Action_Log,*pFile_Action_Log;
+
+typedef struct _XmlFilter
+{
+	int  nIndex;
+	char szSearceName[MAX_PATH];
+	char szSearchPath[MAX_PATH];
+	char szSearchType[MAX_PATH*2];
+	char szExts[MAX_PATH*5];
+}XmlFilter,*pXmlFilter;
 
 typedef struct _SearchRectord
 {
